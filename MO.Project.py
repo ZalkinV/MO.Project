3e62a2b3712c92a62b9f78277528ae786b83bf3b
Zalkin <Zalkin.viktor@mail.ru>
2018-12-21 21:57:57 +0300

    Add test data reading, processing, printing
    I also had to change percentile from 0.7 to 0.85 because otherwise
    no one features  meet this conditions and are deleted

--- MO.Project.py
+++ MO.Project.py
@@ -6,19 +6,24 @@ import xgboost as xgb
 label_name = "target"
 
 def main():
-	data_raw_train = pd.read_csv("../train.csv", index_col="ID", nrows=200)
+	data_raw_train = pd.read_csv("../train.csv", index_col="ID", nrows=800)
+	data_raw_test = pd.read_csv("../test.csv", index_col="ID", nrows=200)
 	
 	labels_train = data_raw_train[label_name]
 	features_train = preprocess_data(data_raw_train.drop(label_name, axis=1))
 	del data_raw_train
 
+	features_test = preprocess_data(data_raw_test)
+	del data_raw_test
+
 	print(features_train)
+	print(features_test)
 	pass
 
 def preprocess_data(data_raw):
 	data_processing = data_raw.copy(deep=True)
 
-	quantile_values = data_processing.quantile(q=0.70)
+	quantile_values = data_processing.quantile(q=0.85)
 	data_processing = data_processing.loc[:, quantile_values > 0]
 	
 	data_processing[:] = preprocessing.minmax_scale(data_processing)
