cd1ec5572d66bcd0931e9e0fa1569ca395d8f8f3
Zalkin <Zalkin.viktor@mail.ru>
2018-12-21 18:36:19 +0300

    Add features processing and printing

--- MO.Project.py
+++ MO.Project.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 def main():
 	data_raw = pd.read_csv("../train.csv", sep=',', index_col="ID", nrows=200)
+	
+	label_name = "target"
+	features_ready = preprocess_data(data_raw.drop(label_name, axis=1))
+
+	print(features_ready)
 	pass
 
 def preprocess_data(data_raw):
