32ab295764c058ab61ba08617c54bf57af843c03
Zalkin <Zalkin.viktor@mail.ru>
2018-12-21 18:35:51 +0300

    Change amount of test data for read from 50 to 200

--- MO.Project.py
+++ MO.Project.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def main():
-	data_raw = pd.read_csv("../train.csv", sep=',', index_col="ID", nrows=500)
-	print(data_raw)
+	data_raw = pd.read_csv("../train.csv", sep=',', index_col="ID", nrows=200)
 	pass
 
 def preprocess_data(data_raw):
