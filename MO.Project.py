8fa0691d1040133ec700bdefff2b03686676020f
Zalkin <Zalkin.viktor@mail.ru>
2018-12-21 18:52:23 +0300

    Rename: data_raw -> data_raw_train, quatiles -> quantile_values

--- MO.Project.py
+++ MO.Project.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def main():
-	data_raw = pd.read_csv("../train.csv", sep=',', index_col="ID", nrows=200)
+	data_raw_train = pd.read_csv("../train.csv", sep=',', index_col="ID", nrows=200)
 	
 	label_name = "target"
-	features_ready = preprocess_data(data_raw.drop(label_name, axis=1))
+	features_ready = preprocess_data(data_raw_train.drop(label_name, axis=1))
 
 	print(features_ready)
 	pass
@@ -12,8 +12,8 @@ def main():
 def preprocess_data(data_raw):
 	data_processing = data_raw.copy(deep=True)
 
-	quantiles = data_processing.quantile(q=0.70)
-	data_processing = data_processing.loc[:, quantiles > 0]
+	quantile_values = data_processing.quantile(q=0.70)
+	data_processing = data_processing.loc[:, quantile_values > 0]
 	
 	return data_processing
 
