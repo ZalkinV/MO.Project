a11a418c02335e51bea5dbbdc8984af569f8ef37
Zalkin <Zalkin.viktor@mail.ru>
2018-12-26 16:05:20 +0300

    Add select_features() inside show_graphs()

--- MO.Project.py
+++ MO.Project.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 from sklearn import model_selection
 
@@ -38,6 +39,22 @@ def main():
 	pass
 
 def show_graphs(features, labels, columns_names=None, n_max=200, rand_seed=0):
+	def select_features():
+		X_train = None
+
+		if columns_names is None:
+			importances = xgb.XGBRegressor().fit(features, labels).feature_importances_
+			importances = importances.reshape(1, importances.size)
+			
+			feature_importance = pd.DataFrame(data=importances, columns=features.columns)
+			feature_importance.sort_values(by=0, axis=1, ascending=False, inplace=True)
+			
+			X_train = features[feature_importance.columns[:4]]
+		else:
+			X_train = features[columns_names[:4]]
+		
+		return X_train
+
 	def select_rows(columns):
 		data_size = 1 - n_max / features.shape[0]
 		data_splitted = model_selection.train_test_split(columns, labels, test_size=data_size, random_state=rand_seed)
