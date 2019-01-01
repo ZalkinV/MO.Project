dc6eb1c8829b57d3290365652aa15c3ea16e3625
Zalkin <Zalkin.viktor@mail.ru>
2019-01-01 18:27:52 +0300

    Change feature selecting by importance

--- GraphPlotter.py
+++ GraphPlotter.py
@@ -7,32 +7,14 @@ import matplotlib.pyplot as plt
 import xgboost as xgb
 
 def show_graphs(features, labels, hypothesis, columns_names=None, n_max=200, rand_seed=0):
-	def select_features():
-		X_train = None
-
-		if columns_names is None:
-			if hasattr(hypothesis, "feature_importances_"):
-				importances = hypothesis.feature_importances_
-			else:
-				importances = build_xgb_regr(features, labels).feature_importances_
-			importances = importances.reshape(1, importances.size)
-			
-			feature_importance = pd.DataFrame(data=importances, columns=features.columns)
-			feature_importance.sort_values(by=0, axis=1, ascending=False, inplace=True)
-			
-			X_train = features[feature_importance.columns[:4]]
-		else:
-			X_train = features[columns_names[:4]]
-		
-		return X_train
-
 	def select_rows(columns):
 		data_size = 1 - n_max / features.shape[0] if n_max < features.shape[0] else 0 
 		data_splitted = model_selection.train_test_split(columns, labels, test_size=data_size, random_state=rand_seed)
 		X_train, y_train = data_splitted[0], data_splitted[2]
 		return X_train, y_train
 
-	picked_features = select_features()
+	feature_importances = select_feature_importances(features, labels, hypothesis=hypothesis)
+	picked_features = features[feature_importances["Feature"][:4]]
 	X, y = select_rows(picked_features)
 
 	fig_dep = plot_dependencies(X, y)
@@ -40,6 +22,21 @@ def show_graphs(features, labels, hypothesis, columns_names=None, n_max=200, ran
 
 	plt.show()
 	pass
+
+def select_feature_importances(features, labels, sort=True, hypothesis=None):
+	if hypothesis is not None and hasattr(hypothesis, "feature_importances_"):
+		importances = hypothesis.feature_importances_
+	else:
+		importances = build_xgb_regr(features, labels).feature_importances_
+			
+	feature_importance = pd.DataFrame()
+	feature_importance.insert(loc=0, column="Importance", value=importances)
+	feature_importance.insert(loc=0, column="Feature", value=features.columns)
+
+	if sort == True:
+		feature_importance.sort_values(by="Importance", axis=0, ascending=False, inplace=True)
+		
+	return feature_importance
 
 def plot_dependencies(X, y):
 	fig, axes = plt.subplots(2, 2)
