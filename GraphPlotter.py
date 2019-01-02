814edd06ea85e1ab1259da3af3e88c4ecc6123c9
Zalkin <Zalkin.viktor@mail.ru>
2019-01-02 22:17:14 +0300

    Add plot_distribution() and its call

--- GraphPlotter.py
+++ GraphPlotter.py
@@ -18,6 +18,7 @@ def show_graphs(features_train, labels, features_test, hypothesis, columns_names
 	X, y = select_rows(picked_features)
 
 	plot_dependencies(X, y)
+	plot_distribution(features_train, features_test, picked_features.columns, range=(0, 0.2))
 	if hasattr(hypothesis, "feature_importances_"):
 		xgb.plot_importance(hypothesis)
 
@@ -53,6 +54,22 @@ def plot_dependencies(X, y):
 		plt.plot(current_feature, y, 'bx', markersize=4)
 
 	return fig
+
+def plot_distribution(features_train, features_test, features_names, range=None):
+	subplot_count = len(features_names)
+	col_count = int(np.sqrt(subplot_count))
+	row_count = int(np.ceil(subplot_count / col_count))
+	
+	fig, axes = plt.subplots(nrows=row_count, ncols=col_count)
+	plt.title("Features distribution")
+	plt.subplots_adjust(wspace=0.4, hspace=0.4)
+	for i, feature_name in enumerate(features_names):
+		plt.subplot(row_count, col_count, i + 1)
+		plt.xlabel(feature_name)
+		plt.ylabel("Count",)
+		plt.hist([features_train[feature_name], features_test[feature_name]], range=range, stacked=True, color=["b","r"])
+
+	pass
 
 def build_xgb_regr(features, labels):
 	return xgb.XGBRegressor().fit(features, labels)
